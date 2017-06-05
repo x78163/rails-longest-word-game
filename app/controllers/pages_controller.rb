b32141b93e832f340fd204b7fd01e2871f4b801c
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-06-05 16:34:04 +0200

    finished

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,82 @@
+require 'open-uri'
+require 'json'
+
+class PagesController < ApplicationController
+
+  def game
+    grid = generate_grid(9)
+
+    @grid_display = grid.join(" ")
+    @start_time = Time.now
+
+
+  end
+
+  def score
+
+   input = params[:answer]
+   start_time = params[:start].to_i
+   grid = params[:grid]
+   end_time = Time.now
+@input = input
+@grid =grid
+@result = run_game(input, grid, start_time, end_time)
+
+
+
+end
+end
+
+
+def generate_grid(grid_size)
+  Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+end
+
+
+def included?(guess, grid)
+  guess.split('').all? { |letter| guess.split('').count(letter) <= grid.count(letter) }
+end
+
+def compute_score(attempt, time_taken)
+  (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+end
+
+def run_game(attempt, grid, start_time, end_time)
+  result = { time: end_time - start_time }
+
+  result[:translation] = get_translation(attempt)
+  result[:score], result[:message] = score_and_message(
+    attempt, result[:translation], grid, result[:time])
+
+  result
+end
+
+def score_and_message(attempt, translation, grid, time)
+  if included?(attempt.upcase, grid)
+    if translation
+      score = compute_score(attempt, time)
+      [score, "well done"]
+    else
+      [0, "not an english word"]
+    end
+  else
+    [0, "not in the grid"]
+  end
+end
+
+def get_translation(word)
+  api_key = "YOUR_SYSTRAN_API_KEY"
+  begin
+    response = open("https://api-platform.systran.net/translation/text/translate?source=en&target=fr&key=#{api_key}&input=#{word}")
+    json = JSON.parse(response.read.to_s)
+    if json['outputs'] && json['outputs'][0] && json['outputs'][0]['output'] && json['outputs'][0]['output'] != word
+      return json['outputs'][0]['output']
+    end
+  rescue
+    if File.read('/usr/share/dict/words').upcase.split("\n").include? word.upcase
+      return word
+    else
+      return nil
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'game', to: 'pages#game'
+
+  get 'score', to: 'pages#score'
+
+  root to: 'pages#game'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
